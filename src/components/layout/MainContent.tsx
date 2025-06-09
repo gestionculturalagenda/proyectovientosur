7a5e353771bc37b6a519f968d4365a4e54172eb4
gestionculturalagenda <gestionculturalagenda@gmail.com>
2025-06-09 03:54:06 +0000

    Sección de sugerencias de perfiles a seguir mejorada y responsive, ajustes de diseño y ubicación en HomePage

--- src/components/layout/MainContent.tsx
+++ src/components/layout/MainContent.tsx
@@ -1,9 +1,5 @@
 const MainContent: React.FC = () => {
-	return (
-		<div className="flex flex-col gap-6 bg-white dark:bg-gray-900 rounded-lg shadow p-4">
-			{/* Aquí iría la sección de historias o el contenido principal real */}
-		</div>
-	);
+	return null;
 };
 
 export default MainContent;
